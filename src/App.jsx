a84facb349c5a3933992e40982fb3151cb068c83
DautSadykov <sadykov.daut@yandex.ru>
2023-05-17 18:23:32 +0500

    received messages update

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
   const apiTokenInstance = '10735b7e0cb2492db24f66f79e059652a937254ba6b84ee7bf'
       
   const [messageText, setMessageText] = React.useState('')
-  const [phoneNumber, setPhoneNumber] = React.useState('79223519045')
+  const [phoneNumber, setPhoneNumber] = React.useState('79651670920')
   
   const outMessageText = {
     chatId: `${phoneNumber}@c.us`,
@@ -20,70 +20,88 @@ export default function App() {
     body: JSON.stringify(outMessageText)
   }
   
-  const [sentMessages, setSentMessages] = React.useState([
-    {
-      id: 1,
-      text: 'recieved message',
-      received: true
-    },
-    {
-      id: 2,
-      text: 'sent message',
-      received: false
-    },
-  ])
+  const [messagesData, setMessagesData] = React.useState([])
 
   function handleMessageChange(event) {
-    setMessageText(() => event.target.value)
+    setMessageText(() => event.target.value) 
   }
 
-  // function recieveMessage() {
-    
-  //   fetch(`${host}/waInstance${idInstance}/receiveNotification/${apiTokenInstance}`)
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       const recievedMessageId = data.receiptId.toString()
-  //       console.log(recievedMessageId)
-  //       // console.log(data.body.messageData)
-  //       console.log(data.body.messageData.textMessageData.textMessage)
-  //       fetch(`${host}/waInstance${idInstance}/deleteNotification/${apiTokenInstance}/${recievedMessageId}`, {method: 'DELETE'})
-  //     })
-  //   }
-
-  // React.useEffect(() => {
-  //   recieveMessage()
-  // })
-
   React.useEffect(() => {
-    const receiveMessages = async () => {
-      let hasMoreMessages = true
+    const apiUrl = `https://api.green-api.com/waInstance${idInstance}/receiveNotification/${apiTokenInstance}`;
 
-      while (hasMoreMessages) {
-        try {
-          const response = await fetch(`${host}/waInstance${idInstance}/receiveNotification/${apiTokenInstance}`)
-          const data = await response.json()
-
-          if (data) {
-            const receivedMessageId = data.receiptId.toString()
-            console.log(data.body.messageData.textMessageData.textMessage)
-            addNewMessage(data.body.messageData.textMessageData.textMessage, receivedMessageId, true)
-            await fetch(`${host}/waInstance${idInstance}/deleteNotification/${apiTokenInstance}/${receivedMessageId}`, { method: 'DELETE' })
-          } else {
-            hasMoreMessages = false 
-          }
-        } catch (error) {
-          console.error('An error occurred while receiving messages:', error)
-        }
-
-        await new Promise(resolve => setTimeout(resolve, 3000))
+    const fetchMessages = async () => {
+      try {
+        const response = await fetch(apiUrl);
+        const message = await response.json();
+        console.log(message)
+        // const receivedMessageId = message.receiptId.toString()
+        // await fetch(`${host}/waInstance${idInstance}/deleteNotification/${apiTokenInstance}/${receivedMessageId}`, { method: 'DELETE' })
+      } catch (error) {
+        console.error('Error fetching messages:', error);
       }
+
+      setTimeout(fetchMessages, 1000);
     };
 
-    receiveMessages();
+    // Start fetching messages
+    fetchMessages();
   }, []);
 
+  // React.useEffect(() => {
+  //   const intervalId = setInterval(() => {
+  //     async function receiveMessage () {
+  //       const response = await fetch(`${host}/waInstance${idInstance}/receiveNotification/${apiTokenInstance}`)
+  //       const data = await response.json()
+  //       console.log('function called')
+  //       if (data) {
+  //         console.log('data received')
+  //         const receivedMessageId = data.receiptId.toString()
+  //         addNewMessage(data.body.messageData.textMessageData.textMessage, receivedMessageId, true)
+  //         await fetch(`${host}/waInstance${idInstance}/deleteNotification/${apiTokenInstance}/${receivedMessageId}`, { method: 'DELETE' })
+  //       }
+  //     }
+  //     receiveMessage()
 
+  //     return () => {
+  //       clearInterval(intervalId);
+  //     };
+  //   }, 2000);
+  // }, []);
+
+  // React.useEffect(() => {
+  //   async function receiveMessages () {
+  //     let hasMoreMessages = true
+
+  //     while (hasMoreMessages) {
+  //       try {
+  //         const response = await fetch(`${host}/waInstance${idInstance}/receiveNotification/${apiTokenInstance}`)
+  //         const data = await response.json()
+
+  //         if (data) {
+  //           const receivedMessageId = data.receiptId.toString()
+  //           const messageExists = messagesData.find((message) => message.id === receivedMessageId)
+  //           if (!messageExists) {
+  //             addNewMessage(data.body.messageData.textMessageData.textMessage, receivedMessageId, true)
+  //           }
+
+  //           await fetch(`${host}/waInstance${idInstance}/deleteNotification/${apiTokenInstance}/${receivedMessageId}`, { method: 'DELETE' })
+  //         } else {
+  //           hasMoreMessages = false 
+  //         }
+  //       } catch (error) {
+  //         console.error('An error occurred while receiving messages:', error)
+  //       }
+
+  //       await new Promise(resolve => setTimeout(resolve, 3000))
+  //     }
+  //   };
+
+  //   receiveMessages();
+  // }, []);
+  
+  
   function handleSendMessage() {
+    if (!messageText) return 1
     fetch(`${host}/waInstance${idInstance}/sendMessage/${apiTokenInstance}`, sendMessageOptions)
     .then(res => res.json())
     .then(data => {
@@ -95,9 +113,9 @@ export default function App() {
   }
 
   function addNewMessage(text, id, isReceived) {
-    setSentMessages((prevSentMessages) => {
+    setMessagesData((prevSentMessages) => {
       const newMessage = {
-        id: id,
+        id: isReceived ? `received_${id}` : id,
         text: text,
         received: isReceived,
       }
@@ -109,6 +127,7 @@ export default function App() {
     const phoneNum = document.getElementById('pn').value
     setPhoneNumber(phoneNum)
     document.getElementById('pn').value = ''
+    setMessagesData(() => [])
   }
 
   return(
@@ -121,7 +140,7 @@ export default function App() {
           handleMessageChange = {handleMessageChange}
           handleSendMessage = {handleSendMessage}
           phoneNumber = {phoneNumber}
-          sentMessages = {sentMessages}
+          sentMessages = {messagesData}
         />
       </div>
     </main>
